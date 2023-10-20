c8118cf13429fe8dc1298d71b690bd83fb09b853
EugeneyCh <eugeney@ua.fm>
2023-10-20 13:57:31 +0300

    Sixth_Trying

--- src/components/Hooks/hooks.js
+++ src/components/Hooks/hooks.js
@@ -2,19 +2,21 @@ import { useEffect, useMemo, useState } from 'react';
 import { createSearchOptions } from 'components/Utilities/utilities';
 import axios from 'axios';
 
-export const useGetFetchImages = (query, currPage) => {
+export const useGetFetchImages = query => {
   const [pictures, setPictures] = useState([]);
   const [totalCount, setTotalCount] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  const getFetchImages = async (query, currPage) => {
+  const getFetchImages = async (query, currentPage) => {
+    console.log('Fetch starting');
     if (!query) {
       //   setPictures([]);
       return;
     }
     setIsLoading(true);
     try {
-      const { data } = await axios.get(createSearchOptions(query, currPage));
+      const { data } = await axios.get(createSearchOptions(query, currentPage));
       const newPictures = data.hits;
       setTotalCount(data.totalHits);
       setPictures(prevPictures => [...prevPictures, ...newPictures]);
@@ -25,22 +27,27 @@ export const useGetFetchImages = (query, currPage) => {
       setIsLoading(false);
     }
   };
-  const clearPages = searchQuery => {
-    if (searchQuery) {
-      // setCurrentPage(() => 1);
+  const clearPages = () => {
+    if (query) {
+      console.log('Pictures and startPage were cleaned');
+      setCurrentPage(() => 1);
       setPictures([]);
     }
   };
-
+  const nextPages = () => {
+    setIsLoading(true);
+    setCurrentPage(currentPage => currentPage + 1);
+    console.log('Current page is...', currentPage);
+  };
   /**
    * memo for clearPages
    */
   // useMemo(() => clearPages(query), [query]);
 
   useEffect(() => {
-    console.log('Query & page equal ', query, currPage);
-    getFetchImages(query, currPage);
-  }, [currPage, query]);
+    // console.log('Query & page equal ', query, currentPage);
+    getFetchImages(query, currentPage);
+  }, [query]);
 
-  return [pictures, isLoading, totalCount, clearPages];
+  return [pictures, isLoading, totalCount, currentPage, clearPages, nextPages];
 };
